332489a846808384042e005011dcaccdf9dd3b96
Dongzhe Wu <dongzhewu@Dongzhes-MacBook-Pro-2.local>
2026-06-30 14:18:41 +0800

    Revert adding agent_skill to repo

--- agent_skill/scripts/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /root/CNN-Fear-And-Greed-Pine
-git pull origin main
-cd agent_skill/scripts
-python3 auto_update.py
-cd ../..
-git add CNN.pine
-git commit -m "Auto update CNN.pine $(date)"
-git push origin main
